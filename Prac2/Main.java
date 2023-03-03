6fd784f3515609f7dc5d71fe905ac13122ba6a79
scrtDfCllctn <125752418+scrtDefColletion@users.noreply.github.com>
2023-03-03 18:43:56 +0300

    Add files via upload

---
+++ Prac2/Main.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Practical task 2. Variant 2. Student Kalakutin S.R. Group RIBO-04-21 \n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter your telephone type (landline/mobile): ");
+        String type = scanner.nextLine();
+        String model, location, owner;
+        int number;
+
+        if(type.equals("mobile"))
+        {
+            System.out.printf("You have chosen a mobile phone, enter its model: ");
+            model = scanner.nextLine();
+            System.out.printf("number: ");
+            number = Integer.parseInt(scanner.nextLine());
+            System.out.printf("owner: ");
+            owner = scanner.nextLine();
+
+            MobilePhone mobilePhone = new MobilePhone(type, model, number, owner);
+            mobilePhone.display();
+        }
+        else if(type.equals("landline"))
+        {
+            System.out.printf("You have chosen a landline phone, enter its model: ");
+            model = scanner.nextLine();
+            System.out.printf("number: ");
+            number = Integer.parseInt(scanner.nextLine());
+            System.out.printf("location: ");
+            location = scanner.nextLine();
+
+            LandlinePhone landlinePhone= new LandlinePhone(type, model, number, location);
+            landlinePhone.display();
+        }
+        else{System.out.printf("Oops, something wrong :(");}
+
+    }
+
+}
